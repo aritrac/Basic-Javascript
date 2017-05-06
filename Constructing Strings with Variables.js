e17d88d384903b8b16ad2d8f1cb8e5bee67994bc
aritrac <aritrachatterjee2007@gmail.com>
2017-05-06 17:55:21 +0530

    Commit number 28

---
+++ Constructing Strings with Variables.js
@@ -0,0 +1,19 @@
+//[COMMENTS]
+/*
+Instructions
+Set myName to a string equal to your name and build myStr with myName between the strings "My name is " and " and I am swell!"
+
+myName should be set to a string at least 3 characters long
+Use two + operators to build myStr with myName inside it
+*/
+//[COMMENTS]
+
+
+// Example
+var ourName = "Free Code Camp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+// Only change code below this line
+var myName = "Aritra Chatterjee";
+var myStr = "My name is " + myName + " and I am swell!";
+
