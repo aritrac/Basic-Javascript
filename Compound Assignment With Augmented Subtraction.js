d7e06649048c084c659fa204b29230f7e9021c29
aritrac <aritrachatterjee2007@gmail.com>
2017-05-06 17:25:57 +0530

    Commit number 18

---
+++ Compound Assignment With Augmented Subtraction.js
@@ -0,0 +1,22 @@
+//[COMMENTS]
+//a should equal 5
+//b should equal -6
+//c should equal 2
+//You should use the -= operator for each variable
+//Do not modify the code above the line
+//[COMMENTS]
+
+
+var a = 11;
+var b = 9;
+var c = 3;
+
+// Only modify code below this line
+
+//a = a - 6;
+//b = b - 15;
+//c = c - 1;
+
+a -= 6;
+b -= 15;
+c -= 1;
