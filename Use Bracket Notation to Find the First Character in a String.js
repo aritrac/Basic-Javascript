7c7cd478604cb265351ef4b2c50c92a45243eb68
aritrac <aritrachatterjee2007@gmail.com>
2017-05-06 18:02:33 +0530

    Commit number 31

---
+++ Use Bracket Notation to Find the First Character in a String.js
@@ -0,0 +1,25 @@
+//[COMMENTS]
+/*
+Instructions
+Use bracket notation to find the first character in the lastName variable and assign it to firstLetterOfLastName.
+
+Hint
+Try looking at the firstLetterOfFirstName variable declaration if you get stuck.
+
+The firstLetterOfLastName variable should have the value of L.
+You should use bracket notation.
+*/
+//[COMMENTS]
+
+// Example
+var firstLetterOfFirstName = "";
+var firstName = "Ada";
+
+firstLetterOfFirstName = firstName[0];
+
+// Setup
+var firstLetterOfLastName = "";
+var lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0];
