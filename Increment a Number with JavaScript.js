7a1e446aeb1cf6a875d2a526877a7688629438ab
aritrac <aritrachatterjee2007@gmail.com>
2017-05-06 17:07:22 +0530

    Commit number 11

---
+++ Increment a Number with JavaScript.js
@@ -0,0 +1,12 @@
+//[COMMENTS]
+//Make the variable quotient equal to 2.
+//Use the / operator
+//[COMMENTS]
+
+
+var myVar = 87;
+
+// Only change code below this line
+//myVar = myVar + 1 <- This was the original line which was replaced by the line below
+myVar++;
+
