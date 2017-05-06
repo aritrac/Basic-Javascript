83a7cb361cee80bf52748129714e49dd90aaa4bf
aritrac <aritrachatterjee2007@gmail.com>
2017-05-06 13:29:18 +0530

    1st commit

---
+++ Comment your JavaScript Code.js
@@ -0,0 +1,11 @@
+//[COMMENTS]
+//Create a // style comment that contains at least five letters.
+//Create a /* */ style comment that contains at least five letters.
+//Make sure that you close the comment with a */.
+//[COMMENTS]
+
+//in-line comment
+
+/*
+multi-line comment
+*/
