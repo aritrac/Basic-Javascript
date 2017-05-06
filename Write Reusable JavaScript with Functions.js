ec6de627cd9ef48075ea362d9892b1e2cc7f98a1
aritrac <aritrachatterjee2007@gmail.com>
2017-05-06 19:01:08 +0530

    Commit number 47

---
+++ Write Reusable JavaScript with Functions.js
@@ -0,0 +1,26 @@
+//[COMMENTS]
+/*
+Instructions
+Create a function called reusableFunction which prints "Hi World" to the dev console.
+Call the function.
+
+reusableFunction should be a function
+reusableFunction should output "Hi World" to the dev console
+Call reusableFunction after you define it
+
+*/
+//[COMMENTS]
+
+// Example
+function ourReusableFunction() {
+  console.log("Heyya, World");
+}
+
+ourReusableFunction();
+
+// Only change code below this line
+function reusableFunction(){
+  console.log("Hi World");
+}
+
+reusableFunction();
