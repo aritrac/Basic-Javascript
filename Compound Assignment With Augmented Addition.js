f7608c90b793a5ed6f1aaf1c22023d999573d3a9
aritrac <aritrachatterjee2007@gmail.com>
2017-05-06 17:23:36 +0530

    Commit number 17

---
+++ Compound Assignment With Augmented Addition.js
@@ -0,0 +1,23 @@
+//[COMMENTS]
+//a should equal 15
+//b should equal 26
+//c should equal 19
+//You should use the += operator for each variable
+//Do not modify the code above the line
+//[COMMENTS]
+
+
+var a = 3;
+var b = 17;
+var c = 12;
+
+// Only modify code below this line
+
+//Original statements in the problem
+//a = a + 12;
+//b = 9 + b;
+//c = c + 7;
+
+a += 12;
+b += 9;
+c += 7;
