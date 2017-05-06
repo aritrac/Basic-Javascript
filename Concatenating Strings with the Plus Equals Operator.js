e23848095de90892fe1d368c64926426b8a167c5
aritrac <aritrachatterjee2007@gmail.com>
2017-05-06 17:53:05 +0530

    Commit number 27

---
+++ Concatenating Strings with the Plus Equals Operator.js
@@ -0,0 +1,20 @@
+//[COMMENTS]
+/*
+Build myStr over several lines by concatenating these two strings:
+"This is the first sentence. " and "This is the second sentence." using the += operator.
+
+myStr should have a value of This is the first sentence. This is the second sentence.
+Use the += operator to build myStr
+*/
+//[COMMENTS]
+
+
+// Example
+var ourStr = "I come first. ";
+ourStr += "I come second.";
+
+// Only change code below this line
+
+var myStr = "This is the first sentence. ";
+myStr += "This is the second sentence.";
+
