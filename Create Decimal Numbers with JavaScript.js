b9bd23529c263b29bb265b6f8c903ca56229f326
aritrac <aritrachatterjee2007@gmail.com>
2017-05-06 17:13:22 +0530

    Commit number 13

---
+++ Create Decimal Numbers with JavaScript.js
@@ -0,0 +1,11 @@
+//[COMMENTS]
+//myDecimal should be a number.
+//myDecimal should have a decimal point
+//[COMMENTS]
+
+
+var ourDecimal = 5.7;
+
+// Only change code below this line
+
+var myDecimal = 6.8;
