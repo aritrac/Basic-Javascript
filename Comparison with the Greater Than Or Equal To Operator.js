bc71e835634575f6a3b8299251a27d4d0d354f0f
aritrac <aritrachatterjee2007@gmail.com>
2017-05-06 19:40:34 +0530

    Commit number 62 again

--- Comparison with the Greater Than Or Equal To Operator.js
+++ Comparison with the Greater Than Or Equal To Operator.js
@@ -2,6 +2,15 @@
 /*
 Instructions
 Add the greater than or equal to operator to the indicated lines so that the return statements make sense.
+
+testGreaterOrEqual(0) should return "9 or Under"
+testGreaterOrEqual(9) should return "9 or Under"
+testGreaterOrEqual(10) should return "10 or Over"
+testGreaterOrEqual(11) should return "10 or Over"
+testGreaterOrEqual(19) should return "10 or Over"
+testGreaterOrEqual(100) should return "20 or Over"
+testGreaterOrEqual(21) should return "20 or Over"
+You should use the >= operator at least twice
 */
 //[COMMENTS]
 
