91d4857470602da8f5414cb43a2b39694a759486
aritrac <aritrachatterjee2007@gmail.com>
2017-05-06 17:40:09 +0530

    Commit number 23

---
+++ Escaping Literal Quotes in Strings.js
@@ -0,0 +1,8 @@
+//[COMMENTS]
+//You should use two double quotes (") and four escaped double quotes (\").
+//Variable myStr should contain the string: I am a "double quoted" string inside "double quotes".
+//[COMMENTS]
+
+
+var myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+
