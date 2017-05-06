5c10e885b96dc2c2a38f0506c82b65ebfc931ec7
aritrac <aritrachatterjee2007@gmail.com>
2017-05-06 17:30:36 +0530

    Commit number 20

---
+++ Compound Assignment With Augmented Division.js
@@ -0,0 +1,22 @@
+//[COMMENTS]
+//a should equal 4
+//b should equal 27
+//c should equal 3
+//You should use the /= operator for each variable
+//Do not modify the code above the line
+//[COMMENTS]
+
+var a = 48;
+var b = 108;
+var c = 33;
+
+// Only modify code below this line
+
+//Original lines from the problem
+//a = a / 12;
+//b = b / 4;
+//c = c / 11;
+
+a /= 12;
+b /= 4;
+c /= 11;
