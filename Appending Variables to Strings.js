00a967fe64ca52b1a5361815ce2100d0bc99dcab
aritrac <aritrachatterjee2007@gmail.com>
2017-05-06 17:58:13 +0530

    Commit number 29

---
+++ Appending Variables to Strings.js
@@ -0,0 +1,20 @@
+//[COMMENTS]
+/*
+Instructions
+Set someAdjective and append it to myStr using the += operator.
+
+someAdjective should be set to a string at least 3 characters long
+Append someAdjective to myStr using the += operator
+*/
+//[COMMENTS]
+
+// Example
+var anAdjective = "awesome!";
+var ourStr = "Free Code Camp is ";
+ourStr += anAdjective;
+
+// Only change code below this line
+
+var someAdjective = "enlightening!";
+var myStr = "Learning to code is ";
+myStr += someAdjective;
