ae6432dc8f8e18dec3637752126bca7310141ede
aritrac <aritrachatterjee2007@gmail.com>
2017-05-06 17:36:32 +0530

    Commit number 22

---
+++ Declare String Variables.js
@@ -0,0 +1,15 @@
+//[COMMENTS]
+//myFirstName should be a string with at least one character in it.
+//myLastName should be a string with at least one character in it.
+//[COMMENTS]
+
+
+// Example
+var firstName = "Alan";
+var lastName = "Turing";
+
+// Only change code below this line
+//The lines below were added
+var myFirstName = "Aritra";
+var myLastName = "Chatterjee";
+
