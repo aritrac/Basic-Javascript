22332f771e1677e2d826321a09e976503849a62f
aritrac <aritrachatterjee2007@gmail.com>
2017-05-06 17:50:50 +0530

    Commit number 26

---
+++ Concatenating Strings with Plus Operator.js
@@ -0,0 +1,19 @@
+//[COMMENTS]
+/*
+Build myStr from the strings "This is the start. " and "This is the end." using the + operator.
+
+myStr should have a value of This is the start. This is the end.
+Use the + operator to build myStr
+myStr should be created using the var keyword.
+Make sure to assign the result to the myStr variable.
+*/
+//[COMMENTS]
+
+
+// Example
+var ourStr = "I come first. " + "I come second.";
+
+// Only change code below this line
+
+var myStr = "This is the start." + " This is the end.";
+
