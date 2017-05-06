326c00d79f00518565a10e3891a6a0a56c17c85b
aritrac <aritrachatterjee2007@gmail.com>
2017-05-06 17:43:43 +0530

    Commit number 24

---
+++ Quoting Strings with Single Quotes.js
@@ -0,0 +1,10 @@
+//[COMMENTS]
+//Remove all the backslashes (\)
+//You should have two single quotes ' and four double quotes "
+//Only remove the backslashes \ used to escape quotes.
+//[COMMENTS]
+
+//The line initially was having backslashes which was removed below
+//var myStr = "<a href=\"http://www.example.com\" target=\"_blank\">Link</a>";
+var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
