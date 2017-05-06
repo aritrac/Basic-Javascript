9dfb17cbc3803b1e7c923eeaf80391ce9d6ce9b2
aritrac <aritrachatterjee2007@gmail.com>
2017-05-06 17:21:10 +0530

    Commit number 16

---
+++ Finding a Remainder in JavaScript.js
@@ -0,0 +1,11 @@
+//[COMMENTS]
+//The variable remainder should be initialized
+//The value of remainder should be 2
+//You should use the % operator
+//[COMMENTS]
+
+
+// Only change code below this line
+
+var remainder = 7 % 5;
+
