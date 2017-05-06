5562f057816fc94f9217fe24c44bfe2d08fae046
aritrac <aritrachatterjee2007@gmail.com>
2017-05-06 13:47:27 +0530

    Commit number 6

---
+++ Understanding Case Sensitivity in Variables.js
@@ -0,0 +1,18 @@
+//[COMMENTS]
+//studlyCapVar is defined and has a value of 10
+//properCamelCase is defined and has a value of "A String"
+//titleCaseOver is defined and has a value of 9000
+//studlyCapVar should use camelCase in both declaration and assignment sections.
+//properCamelCase should use camelCase in both declaration and assignment sections.
+//titleCaseOver should use camelCase in both declaration and assignment sections.
+//[COMMENTS]
+
+// Declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
