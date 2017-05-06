d844c2d950d2af0864052693125714e2851b803c
aritrac <aritrachatterjee2007@gmail.com>
2017-05-06 17:28:28 +0530

    Commit number 19

---
+++ Compound Assignment With Augmented Multiplication.js
@@ -0,0 +1,23 @@
+//[COMMENTS]
+//a should equal 25
+//b should equal 36
+//c should equal 46
+//You should use the *= operator for each variable
+//Do not modify the code above the line
+//[COMMENTS]
+
+
+var a = 5;
+var b = 12;
+var c = 4.6;
+
+// Only modify code below this line
+
+//Original lines from the problem
+//a = a * 5;
+//b = 3 * b;
+//c = c * 10;
+
+a *= 5;
+b *= 3;
+c *= 10;
