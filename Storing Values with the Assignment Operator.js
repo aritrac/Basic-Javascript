1f8342b094c68addbd1c05a60390f8404934dcdc
aritrac <aritrachatterjee2007@gmail.com>
2017-05-06 13:39:10 +0530

    Commit number 3

---
+++ Storing Values with the Assignment Operator.js
@@ -0,0 +1,14 @@
+//[COMMENTS]
+//Do not change code above the line
+//a should have a value of 7
+//b should have a value of 7
+//a should be assigned to b with =
+//[COMMENTS]
+
+// Setup
+var a;
+var b = 2;
+
+// Only change code below this line
+a = 7;
+b = a;
